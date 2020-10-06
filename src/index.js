1ce9270713a04eaf38f150487d2def01171390d7
Misha Pateev <tokensmix@gmail.com>
2020-10-06 17:28:48 +0300

    feat: did task js brackets

--- src/index.js
+++ src/index.js
@@ -7,25 +7,37 @@ module.exports = function check(str, arr) {
     
   
     for(let i = 0; i < arr.length; i++){
-      for(let j = 0; j < 1; j++){
-        opened.push(arr[i][0].trim());
-        closed.push(arr[i][1].trim());
+      for(let j = 0; j < 1; j++){   
       }
     }
     
     
     for(let i = 0; i < str.length; i++){
       char = str[i];
-  
-      if(char !== ' ' && opened.indexOf(char) > -1){
-        checkBrackets.push(char);   
-      }
-      else if(closed.indexOf(char) > -1){
-        bracketSimple = opened[closed.indexOf(char)];
-        if(bracketSimple !== checkBrackets.pop()){
-          return false; 
+
+      if(opened.includes(char) === closed.includes(char)){
+        if(char !== ' ' & !checkBrackets.includes(char)){
+          checkBrackets.push(char);
+        } else if(checkBrackets.length === 0 || closed.indexOf(char) > -1){
+          bracketSimple = opened[closed.indexOf(char)];
+          if(bracketSimple !== checkBrackets.pop()){
+            return false; 
+          }
         }
-      } 
+      }
+      else{
+        if(char !== ' ' && opened.indexOf(char) > -1){
+          checkBrackets.push(char); 
+        
+          
+        }
+        else if(checkBrackets.length === 0 || closed.indexOf(char) > -1){
+          bracketSimple = opened[closed.indexOf(char)];
+          if(bracketSimple !== checkBrackets.pop()){
+            return false; 
+          }
+        }
+      }  
     
     }
      
