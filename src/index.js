3f7fa63ca07f591acf0b9c5fd7b2c1f3c678c822
Misha Pateev <tokensmix@gmail.com>
2020-10-06 17:38:51 +0300

    feat: did task js brackets

--- src/index.js
+++ src/index.js
@@ -7,15 +7,18 @@ module.exports = function check(str, arr) {
     
   
     for(let i = 0; i < arr.length; i++){
-      for(let j = 0; j < 1; j++){   
+      for(let j = 0; j < 1; j++){
+        opened.push(arr[i][0].trim());
+        closed.push(arr[i][1].trim());
+        
       }
     }
     
     
     for(let i = 0; i < str.length; i++){
       char = str[i];
-
       if(opened.includes(char) === closed.includes(char)){
+       
         if(char !== ' ' & !checkBrackets.includes(char)){
           checkBrackets.push(char);
         } else if(checkBrackets.length === 0 || closed.indexOf(char) > -1){
